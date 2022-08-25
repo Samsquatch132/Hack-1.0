8f26a395d77d881a6eeab59b1f7f8ba693de70d8
Sam Hartman <shartman16@huskers.unl.edu>
2022-08-25 19:08:43 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Sam Hartman
+ * Date: 2022/8/25
+ *
+ *// source file with my name
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Sam Hartman\n");
+
+  return 0;
+}
